abc1f2670d8ddbb9fcf6362a3fe28aeed2ed60e8
Anurag <anurag.astikint@gmail.com>
2021-12-03 19:34:43 +0530

    Added Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './sass/main.scss'
+
+import Navbar from './components/navbar/Navbar.jsx'
 
 const App = () => {
   return (
     <>
-      yo wassup dog!
+      <Navbar/>
     </>
   )
 }
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.scss'
+
+const Navbar = () => {
+    return (
+        <>
+            <div id="navbar">
+                <span id='logoText'>PRIYA FASHION</span>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
