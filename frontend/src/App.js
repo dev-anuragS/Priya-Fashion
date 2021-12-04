dd8b65fd2f64caa2f5a25e89d507203e5b506b71
Anurag <anurag.astikint@gmail.com>
2021-12-04 21:57:55 +0530

    Edited navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './sass/main.scss'
 
-import Navbar from './components/navbar/Navbar.jsx'
+import Navbar from './components/navbar/Navbar.jsx';
+import Home from './components/home/Home.jsx';
+
+import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </>
   )
 }
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './navbar.scss'
 
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
     return (
         <>
             <div id="navbar">
                 <span id='logoText'>PRIYA FASHION</span>
+                <ul id='navbarLinks'>
+                    <NavLink className='navbarNavLink' exact to="/"><li>Home</li></NavLink>
+                    <NavLink className='navbarNavLink' exact to="/about"><li>About</li></NavLink>
+                    <NavLink className='navbarNavLink' exact to='/gallery'><li>Gallery</li></NavLink>
+                </ul>
+                <NavLink id='navbarContactLink' exact to='/contact'>Contact</NavLink>
             </div>
         </>
     )
